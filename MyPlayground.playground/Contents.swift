97c769d28501451e3fefd5f9cf848ed6d512cb93
GustavoYamauchi <54085161+GustavoYamauchi@users.noreply.github.com>
2020-02-21 17:55:41 -0300

    quase pronto

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,22 +1,23 @@
 import UIKit
 import PlaygroundSupport
 
-struct teste{
-    var g: UILabel
-    
-    init(g: UILabel){
-        self.g = g
-        adjust()
+
+class MinhaLabel : UILabel{
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        desenharLabel()
     }
     
-    func adjust(){
-        g.backgroundColor = #colorLiteral(red: 0.4, green: 0.3607843137, blue: 0.6470588235, alpha: 1)
-        g.layer.cornerRadius = 20
-        g.layer.masksToBounds = true
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        desenharLabel()
     }
     
-    func ref() -> UILabel{
-        return (g)
+    private func desenharLabel(){
+        backgroundColor = #colorLiteral(red: 0.4, green: 0.3607843137, blue: 0.6470588235, alpha: 1)
+        layer.cornerRadius = 20
+        layer.masksToBounds = true
+        font = UIFont.boldSystemFont(ofSize:  25.0)
     }
 }
 
@@ -34,7 +35,7 @@ class MeuBtn : UIButton{
     
     private func desenharBtn(){
         backgroundColor = .init(red:0.3, green:0.3, blue: 0.3, alpha: 1.0)
-        
+        titleLabel?.font = UIFont.boldSystemFont(ofSize: 25.0)
         layer.cornerRadius = 10
         layer.borderWidth = 1.0
         layer.borderColor = UIColor.darkGray.cgColor
@@ -44,14 +45,12 @@ class MeuBtn : UIButton{
 
 class testeViewController : UIViewController //Controller -> Controla a view
 {
-    var label : UILabel!; //Declara Label
-    var t = teste(g: UILabel())
+    var label : MinhaLabel!; //Declara Label
     
 //    static var label : UILabel? = UILabel()
     var btnV : MeuBtn!;
     var btnM : MeuBtn!;
-    var btnN : MeuBtn!;
-    var btnR : MeuBtn!;
+
     
     let perguntas = ["1 - Pergunta1", "2 - Pergunta2", "3 - Pergunta3", "4 - Pergunta4", "5 - Pergunta5", "6 - Pergunta6"]
     let respostasCorretas = [1, 0, 0, 1, 1, 0]
@@ -63,60 +62,39 @@ class testeViewController : UIViewController //Controller -> Controla a view
         let view = UIView();     //Instancia view
         view.backgroundColor = #colorLiteral(red: 0.1725490196, green: 0.1725490196, blue: 0.1725490196, alpha: 1);
         view.layer.borderColor = #colorLiteral(red: 0.7843137255, green: 0.8470588235, blue: 0.4117647059, alpha: 1)
-        view.layer.borderWidth = 10
+        view.layer.borderWidth = 1
         view.layer.cornerRadius = 20//Set a cor de fundo
         
             
-        let label = t.g          //Instacia Label
+        label = MinhaLabel()          //Instacia Label
         label.text = perguntas[perguntaIndex];     //Set texto
-        label.textColor = #colorLiteral(red: 0.7843137255, green: 0.8470588235, blue: 0.4117647059, alpha: 1);   //Set cor do texto
+        label.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1);   //Set cor do texto
         label.textAlignment = .center
-        label.backgroundColor = #colorLiteral(red: 0.4, green: 0.3607843137, blue: 0.6470588235, alpha: 1)
-        label.layer.cornerRadius = 20
-//        label.layer.borderWidth = 2
-        label.layer.masksToBounds = true
-//        label.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        label.font = UIFont.systemFont(ofSize:  20)
+        
         view.addSubview(label);     //Adiciono a label na view
         
         //config botao de Verdade
         btnV = MeuBtn()
         btnV.setTitle("Verdade", for: .normal);
-        btnV.setTitleColor(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1), for: .normal)
+        btnV.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
         btnV.tag = 1
-        btnV.backgroundColor = .white;
+        btnV.backgroundColor = #colorLiteral(red: 0.4, green: 0.3607843137, blue: 0.6470588235, alpha: 1);
         view.addSubview(btnV)
         
         //config botao de Mentira
         btnM = MeuBtn()
         btnM.setTitle("Mentira", for: .normal);
-        btnM.tintColor = .black
+        btnM.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
         btnM.tag = 0
-        btnM.backgroundColor = .white;
+        btnM.backgroundColor = #colorLiteral(red: 0.4, green: 0.3607843137, blue: 0.6470588235, alpha: 1);
         view.addSubview(btnM)
         
-        //config botao de Proximo
-        btnN = MeuBtn()
-        btnN.setTitle("Proximo", for: .normal);
-        btnN.tintColor = .black
-        btnN.backgroundColor = .white;
-        btnN.isHidden = true
-        view.addSubview(btnN)
-        
-        //config botao de Tentar Novamente
-        btnR = MeuBtn()
-        btnR.setTitle("retry", for: .normal);
-        btnR.tintColor = .black
-        btnR.backgroundColor = .white;
-        btnR.isHidden = true
-        view.addSubview(btnR)
         
         //Autoresinzing
         label.translatesAutoresizingMaskIntoConstraints = false;   //
         btnV.translatesAutoresizingMaskIntoConstraints  = false;
         btnM.translatesAutoresizingMaskIntoConstraints  = false;
-        btnN.translatesAutoresizingMaskIntoConstraints  = false;
-        btnR.translatesAutoresizingMaskIntoConstraints  = false;
+
         
         let margins = view.layoutMarginsGuide; //Pego os valores de margem do sistema (tela)
               
@@ -124,29 +102,25 @@ class testeViewController : UIViewController //Controller -> Controla a view
             label.leadingAnchor.constraint(equalTo: margins.leadingAnchor,constant: 5),    //Define os valores da posição do eixo X
             label.trailingAnchor.constraint(equalTo: margins.trailingAnchor,constant: -5),//Define os valores da posição do eixo X
             label.topAnchor.constraint(equalTo:margins.topAnchor,constant: 25),     //Define os valores da posição do eixo Y
-            label.heightAnchor.constraint(equalToConstant: 250),
-//            label.widthAnchor.constraint(equalToConstant: 10),
+            label.heightAnchor.constraint(equalToConstant: 300),
+            label.widthAnchor.constraint(equalToConstant: 10),
             
-            btnV.leadingAnchor.constraint(equalTo: margins.leadingAnchor,constant: 30),    //Define os valores da posição do eixo X
-            btnV.topAnchor.constraint(equalTo:margins.topAnchor, constant: 600),     //Define os valores da posição do eixo Y
-      
+            btnV.leadingAnchor.constraint(equalTo: margins.leadingAnchor,constant: 5),    //Define os valores da posição do eixo X
+            btnV.topAnchor.constraint(equalTo:margins.centerYAnchor, constant: 60), //Define os valores da posição do eixo Y
+            btnV.trailingAnchor.constraint(equalTo: margins.centerXAnchor, constant: -5),
+            btnV.heightAnchor.constraint(equalToConstant: 200),
             
-            btnM.leadingAnchor.constraint(equalTo: margins.leadingAnchor,constant: 100),    //Define os valores da posição do eixo X
-            btnM.topAnchor.constraint(equalTo:margins.topAnchor, constant: 600),     //Define os valores da posição do eixo Y
-            
-            
-            btnN.leadingAnchor.constraint(equalTo: margins.leadingAnchor,constant: 200),    //Define os valores da posição do eixo X
-            btnN.topAnchor.constraint(equalTo:margins.topAnchor, constant: 600),
-            
-            btnR.leadingAnchor.constraint(equalTo: margins.leadingAnchor,constant: 200),    //Define os valores da posição do eixo X
-            btnR.topAnchor.constraint(equalTo:margins.topAnchor, constant: 600),     //Define os valores da posição do eixo Y//Define os valores da posição do eixo Y
+            btnM.leadingAnchor.constraint(equalTo: margins.centerXAnchor, constant: 5),    //Define os valores da posição do eixo X
+            btnM.topAnchor.constraint(equalTo:margins.centerYAnchor, constant: 60),     //Define os valores da posição do eixo Y
+            btnM.widthAnchor.constraint(equalToConstant: 150),
+            btnM.trailingAnchor.constraint(equalTo: label.trailingAnchor),
+            btnM.heightAnchor.constraint(equalToConstant: 200),
 
         ])
         
         btnV.addTarget(self, action: #selector(verificar), for: .touchUpInside)
         btnM.addTarget(self, action: #selector(verificar), for: .touchUpInside)
-        btnN.addTarget(self, action: #selector(prox), for: .touchUpInside)
-        btnR.addTarget(self, action: #selector(retry), for: .touchUpInside)
+        
         
         
         
@@ -157,44 +131,43 @@ class testeViewController : UIViewController //Controller -> Controla a view
     @objc func verificar(sender: UIButton) {
 
         if (respostasCorretas[perguntaIndex] == sender.tag) {
-            correta();
+            correta(sender: sender);
+            
         }
         else{
             incorreta();
         }
     }
     
-    @objc func prox(){
+    @objc func prox(sender: UIButton){
         perguntaIndex += 1;
-        let label = t.g
         label.text = perguntas[perguntaIndex];
-        label.backgroundColor = .gray
-        btnN.isHidden = true
-        btnV.isHidden = false
-        btnM.isHidden = false
+        label.backgroundColor = #colorLiteral(red: 0.4, green: 0.3607843137, blue: 0.6470588235, alpha: 1)
     }
     
     
     @objc func retry(){
-//        label.backgroundColor = .gray
-        btnR.isHidden = true
-        btnV.isHidden = false
-        btnM.isHidden = false
+       label.backgroundColor = #colorLiteral(red: 0.4, green: 0.3607843137, blue: 0.6470588235, alpha: 1)
+//        btnV.isHidden = false
+//        btnM.isHidden = false
     }
     
-    func correta(){
-//        label.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
-        btnN.isHidden = false
-        btnV.isHidden = true
-        btnM.isHidden = true
+    @objc func correta(sender: UIButton){
+//        btnV.isHidden = true
+//        btnM.isHidden = true
+        self.label.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false, block: { timer in
+            self.prox(sender: sender)
+        })
+        
         print("correto");
     }
     
     func incorreta(){
-//        label.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
-        btnR.isHidden = false
-        btnV.isHidden = true
-        btnM.isHidden = true
+       label.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+//        btnV.isHidden = true
+//        btnM.isHidden = true
         print("incorreto")
     }
     
