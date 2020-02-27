88dfaa634146e2dbdb7d734bbc339890452e3edd
GustavoYamauchi <54085161+GustavoYamauchi@users.noreply.github.com>
2020-02-27 18:13:02 -0300

    um dos ultimos(talvez)

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -50,11 +50,13 @@ class testeViewController : UIViewController //Controller -> Controla a view
 //    static var label : UILabel? = UILabel()
     var btnV : MeuBtn!;
     var btnM : MeuBtn!;
-    var btnInicio : MeuBtn!;
+    var btnInicio, btnFinal : MeuBtn!;
 
     
-    let perguntas = ["Comeco", "1 - Pergunta1", "2 - Pergunta2", "3 - Pergunta3", "4 - Pergunta4", "5 - Pergunta5", "6 - Pergunta6", "Fim"]
-    let respostasCorretas = [3, 1, 1, 1, 1, 1, 1, 1]
+
+    let perguntas = ["Um quiz sobre a Maria Luiza", "O nome completo dela é Maria Luiza Harumi Hioki", "Ela nasceu em 20/02/2000", "Seu primeiro curso foi Sistemas de Informação", "Quando criança queria ser bailarina", "Ela dança há 14 anos", "Durante o ensino médio ganhou dois Óscares Acadêmicos de matemática", "Sua banda favorita é Panic! At The Disco", "Antes de vir para São Paulo morava em Osasco", "Desafio concluido"]
+    let respostasCorretas = [3, 0, 0, 1, 0, 1, 0, 1, 1, 3]
+    
     
     var perguntaIndex = 0;
     
@@ -71,6 +73,8 @@ class testeViewController : UIViewController //Controller -> Controla a view
         label.text = perguntas[perguntaIndex];     //Set texto
         label.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1);   //Set cor do texto
         label.textAlignment = .center
+        label.numberOfLines = 0;
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
 //        salabel.isHidden = true
         
         view.addSubview(label);     //Adiciono a label na view
@@ -95,12 +99,23 @@ class testeViewController : UIViewController //Controller -> Controla a view
 
         //config botao de Inicio
         btnInicio = MeuBtn()
-        btnInicio.setTitle("Inicio", for: .normal);
+        btnInicio.setTitle("Start", for: .normal);
+        btnInicio.setImage(UIImage(named: "images.png"), for: .normal)
+//        btnInicio.backgroundColor(UIColor(patternImage: UIImage(named: "images.png")!))
         btnInicio.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
         btnInicio.tag = 3
         btnInicio.backgroundColor = #colorLiteral(red: 0.4, green: 0.3607843137, blue: 0.6470588235, alpha: 1);
         btnInicio.isHidden = false
         view.addSubview(btnInicio)
+        
+        //config botao de Inicio
+        btnFinal = MeuBtn()
+        btnFinal.setTitle("Inicio", for: .normal);
+        btnFinal.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+        btnFinal.tag = 3
+        btnFinal.backgroundColor = #colorLiteral(red: 0.4, green: 0.3607843137, blue: 0.6470588235, alpha: 1);
+        btnFinal.isHidden = true
+        view.addSubview(btnFinal)
 
 
         //Autoresinzing
@@ -108,6 +123,7 @@ class testeViewController : UIViewController //Controller -> Controla a view
         btnV.translatesAutoresizingMaskIntoConstraints  = false;
         btnM.translatesAutoresizingMaskIntoConstraints  = false;
         btnInicio.translatesAutoresizingMaskIntoConstraints = false;
+        btnFinal.translatesAutoresizingMaskIntoConstraints = false;
 
         
         let margins = view.layoutMarginsGuide; //Pego os valores de margem do sistema (tela)
@@ -116,7 +132,7 @@ class testeViewController : UIViewController //Controller -> Controla a view
             label.leadingAnchor.constraint(equalTo: margins.leadingAnchor,constant: 5),    //Define os valores da posição do eixo X
             label.trailingAnchor.constraint(equalTo: margins.trailingAnchor,constant: -5),//Define os valores da posição do eixo X
             label.topAnchor.constraint(equalTo:margins.topAnchor,constant: 25),     //Define os valores da posição do eixo Y
-            label.heightAnchor.constraint(equalToConstant: 300),
+            label.heightAnchor.constraint(equalToConstant: 350),
             label.widthAnchor.constraint(equalToConstant: 10),
             
             btnV.leadingAnchor.constraint(equalTo: margins.leadingAnchor,constant: 5),    //Define os valores da posição do eixo X
@@ -130,64 +146,76 @@ class testeViewController : UIViewController //Controller -> Controla a view
             btnM.trailingAnchor.constraint(equalTo: label.trailingAnchor),
             btnM.heightAnchor.constraint(equalToConstant: 200),
             
-            
-            
+
             btnInicio.topAnchor.constraint(equalTo: margins.topAnchor, constant: 500),
-            btnInicio.heightAnchor.constraint(equalToConstant: 50),
+            btnInicio.heightAnchor.constraint(equalToConstant: 100),
+            btnInicio.widthAnchor.constraint(equalToConstant: 100),
             btnInicio.centerXAnchor.constraint(equalTo: margins.centerXAnchor),
+            
+            btnFinal.topAnchor.constraint(equalTo: margins.topAnchor, constant: 500),
+            btnFinal.heightAnchor.constraint(equalToConstant: 100),
+            btnFinal.widthAnchor.constraint(equalToConstant: 100),
+            btnFinal.centerXAnchor.constraint(equalTo: margins.centerXAnchor),
         
         ])
         btnV.addTarget(self, action: #selector(verificar), for: .touchUpInside)
         btnM.addTarget(self, action: #selector(verificar), for: .touchUpInside)
         btnInicio.addTarget(self, action: #selector(verificar), for: .touchUpInside)
+        btnFinal.addTarget(self, action: #selector(retry), for: .touchUpInside)
         
         self.view = view;  //Define a view como a view principal
     }
     
     @objc func verificar(sender: UIButton) {
-        
-        
-        
+
         if (respostasCorretas[perguntaIndex] == sender.tag) {
             correta(sender: sender);
         }
         else{
-            incorreta();
+            incorreta(sender: sender);
         }
     }
     
     @objc func prox(sender: UIButton){
-        perguntaIndex += 1;
+        
         
         print(perguntaIndex)
-        if (sender.tag == 3){
+        
+        if(perguntaIndex == -1){
+            btnM.isHidden = true
+            btnV.isHidden = true
+            btnInicio.isHidden = false
+        }
+        else if (sender.tag == 3){
             btnM.isHidden = false
             btnV.isHidden = false
             btnInicio.isHidden = true
         }
-        else if (perguntaIndex == 7){
+        else if (perguntaIndex == 8){
             btnM.isHidden = true
             btnV.isHidden = true
-            btnInicio.isHidden = false
-            perguntaIndex = 0
-                    
+            btnInicio.isHidden = true
+            btnFinal.isHidden = false
         }
         else{
             btnV.isHidden = false
             btnM.isHidden = false
         }
+        
+        perguntaIndex += 1;
         label.text = perguntas[perguntaIndex];
         label.backgroundColor = #colorLiteral(red: 0.4, green: 0.3607843137, blue: 0.6470588235, alpha: 1)
     }
     
     
-    @objc func retry(){
-       label.backgroundColor = #colorLiteral(red: 0.4, green: 0.3607843137, blue: 0.6470588235, alpha: 1)
+    @objc func retry(sender: UIButton){
+        perguntaIndex = -1
+        btnFinal.isHidden = true
+        prox(sender: sender)
     }
     
     @objc func correta(sender: UIButton){
-//        btnV.isHidden = true
-//        btnM.isHidden = true
+
         self.label.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
     
         if (sender.tag == 1){
@@ -199,15 +227,14 @@ class testeViewController : UIViewController //Controller -> Controla a view
         Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false, block: { timer in
             self.prox(sender: sender)
         })
-        
-        print("correto");
     }
     
-    func incorreta(){
+    func incorreta(sender: UIButton){
        label.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
-//        btnV.isHidden = true
-//        btnM.isHidden = true
-        print("incorreto")
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false, block: { timer in
+            self.retry(sender: sender)
+        })
     }
     
     
